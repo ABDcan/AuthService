27bde6c333b9ba2c17cbed746968972511a7e49d
Abhishe <lcs2021036@iiitl.ac.in>
2024-12-20 05:57:04 +0530

    Added roles and user_roles through tables

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./src/config/serverConfig');
 const apiRoutes = require('./src/routes/index');
 const userService = require('./src/services/user-service');
+
+const {User,Role} = require('./src/models/index');
 // const userRepository = require('./src/repository/user-repository');
+const db = require('./src/models/index');
 const prepareAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',apiRoutes);
+    if(process.env.DB_SYNC){
+        db.sequelize.sync({alter:true});
+    }
+
+    const u1 = await User.findByPk(3);
+    const r1 = await Role.findByPk(2);
+    u1.addRole(r1);
     //  const repository = new userRepository();
     // const response = await repository.getById(4);
     // console.log(response);
--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,5 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     PORT:process.env.PORT,
-    JWT_KEY:process.env.JWT_KEY
+    JWT_KEY:process.env.JWT_KEY,
+    DB_SYNC:process.env.DB_SYNC,
 }
